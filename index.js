dc297c5d4a8c17639e0bed7c1022790eaa5a7141
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-12-26 19:05:35 +0530

    implemented middlewares and some new basic http servers to practice

--- index.js
+++ index.js
@@ -7,6 +7,72 @@ const port = 3000
 // Middleware to parse JSON
 app.use(express.json());
 
+var users = [{
+  name: "George",
+  kidneys: [{
+    healthy: false
+  }]
+}];
+
+app.get("/getKidneyInfo", (request, response)=>{
+      const georgeKidneys = users[0].kidneys;
+      const noOfKidneys = georgeKidneys.length;
+
+      const healthyKidneys = georgeKidneys.filter((kidneyOBJ) => kidneyOBJ.healthy === true)
+
+      const numberOfHealthyKidneys = healthyKidneys.length;
+      const numberOfUnhealthyKidneys = georgeKidneys.length - healthyKidneys.length;
+
+      response.json({noOfKidneys, numberOfHealthyKidneys, numberOfUnhealthyKidneys});
+})
+
+app.post("/addNewKidney", (request, response)=>{
+    const healthStatus = Boolean(request.body.healthStatus);
+
+    users[0].kidneys.push({healthy: healthStatus});
+    console.log(users[0].kidneys);
+
+    response.send("kidney added successfully!!!!");
+})
+
+app.put("/replaceKidney", (request, response)=>{
+     
+  let check  = false;
+  for(let i = 0; i < users[0].kidneys.length; i++){
+         if(!users[0].kidneys[i].healthy){
+         users[0].kidneys[i].healthy = true;
+         check = true;
+         }
+     }
+     if(!check){
+      response.status(411).send("You have no unhealthy kidneys to replace");
+     } else {
+       response.send("kidney replaced successfully!!!!");
+     }
+})
+
+app.delete("/deleteKidney", (request,reponse)=>{
+
+   //return 411 if no unhealthy kidneys exist
+
+   let check = false;
+    for(let i = users[0].kidneys.length-1; i >= 0 ; i--){
+      if(users[0].kidneys[i].healthy === false){
+        check = true;
+        users[0].kidneys.splice(i, 1);
+      }
+    }
+    
+    if(!check) {
+      reponse.status(411).send("No unhealthy kidneys exist");
+    } else {
+      reponse.send("Unhealthy kidneys deleted successfully");
+    }
+    
+})
+
+
+
 //creating new route handler for GET requests
 app.get('/route-handler', (req, res)=>{
 
